1b03e292021dcff88fb19d507e06be45a06bdbae
Ashutosh Kumar <kashutosh02@gmail.com>
2024-11-11 15:45:06 +0530

    starting

---
+++ tsa/app/dashboard/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './page.module.css';
+
+const Dashboard = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Dashboard</h1>
+      <p>This is the Dashboard page.</p>
+      <Link href="/">Go back to Home</Link>
+    </div>
+  );
+};
+
+export default Dashboard;
