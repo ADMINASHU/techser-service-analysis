5e5fe6c2d9c012dffb9ca101d8a5b10b61f6314d
Ashutosh Kumar <kashutosh02@gmail.com>
2024-11-18 10:00:21 +0530

    start at office

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -3,20 +3,25 @@ import styles from "./RegisterForm.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const [userID, setUserID] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const formData = new FormData(event.currentTarget);
-      const userID = formData.get("userId");
-      const email = formData.get("email");
-      const password = formData.get("password");
       const response = await axios.post("/api/register", { userID, email, password });
-      response.status === 201 && router.push("/");
+      if (response.status === 201) {
+        setUserID("");
+        setEmail("");
+        setPassword("");
+        router.push("/");
+      }
     } catch (error) {
       console.log(error.message);
     }
@@ -26,9 +31,9 @@ const RegisterForm = () => {
     <div className={styles.registerContainer}>
       <img src="/logo.jpg" alt="Company Logo" className={styles.logo} />
       <form onSubmit={handleSubmit}>
-        <input type="text" id="userId" name="userId" placeholder="User ID" required />
-        <input type="email" id="email" name="email" placeholder="Email" required />
-        <input type="password" id="password" name="password" placeholder="Password" required />
+        <input type="text" id="userId" name="userId" placeholder="User ID" value={userID} onChange={(e)=>setUserID(e.target.value)} required />
+        <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}  required />
+        <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}  required />
         <button type="submit">Signup</button>
         <p>
           Already have an account?
