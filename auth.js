b6c7c0f62bfc7e86a6eefee94efd89b7ee776588
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-18 17:02:04 +0530

    login part fixed

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ import { SignInSchema } from "./lib/zod";
 import bcrypt from "bcryptjs";
 import { signInCredentials } from "./app/action";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth, unstable_update } = NextAuth({
   pages: {
     signIn: "/login",
   },
@@ -22,7 +22,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         if (user.error) {
           throw new CredentialsSignin(user.error);
         }
-        console.log(user);
+        // console.log(user);
 
         if (user) {
           return user;
@@ -32,4 +32,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        const newToken = {
+          userID: user.userID,
+          isAdmin: user.isAdmin,
+          level: user.level,
+          verified: user.verified,
+          fName: user.fName,
+        };
+        token = { ...token, ...newToken };
+      }
+      // console.log(JSON.stringify(token));
+      return token;
+    },
+    session({ session, token }) {
+      session.user = token;
+      console.log("session: "+ JSON.stringify(session));
+      console.log("token: "+ JSON.stringify(token));
+      return session;
+    },
+  },
 });
