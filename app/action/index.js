56b0b9e920562ad973755e9cc2cb949ede81d569
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-19 16:39:59 +0530

    test

--- app/action/index.js
+++ app/action/index.js
@@ -51,50 +51,50 @@ export async function doLogin({ userID, password }) {
 }
 
 export const signUp = async ({ data }) => {
-  // Validate data using Zod schema
-  const result = RegisterSchema.safeParse({
-    userID: data.userID,
-    email: data.email,
-    password: data.password,
-    confirmPassword: data.confirmPassword,
-  });
+  // // Validate data using Zod schema
+  // const result = RegisterSchema.safeParse({
+  //   userID: data.userID,
+  //   email: data.email,
+  //   password: data.password,
+  //   confirmPassword: data.confirmPassword,
+  // });
 
-  if (!result.success) {
-    return { error: result.error.errors[0].message };
-  }
+  // if (!result.success) {
+  //   return { error: result.error.errors[0].message };
+  // }
 
-  try {
-    const db = await connectToServiceEaseDB();
-    if (!db) {
-      return { error: "Error connecting to the database" };
-    }
+  // try {
+  //   const db = await connectToServiceEaseDB();
+  //   if (!db) {
+  //     return { error: "Error connecting to the database" };
+  //   }
 
-    const { userID, email, password } = result.data;
+  //   const { userID, email, password } = result.data;
 
-    // Check if user already exists
-    const existingUserID = await User.findOne({ userID });
-    const existingUserEmail = await User.findOne({ email });
-    if (existingUserID || existingUserEmail) {
-      return { error: "User already exists" };
-    }
+  //   // Check if user already exists
+  //   const existingUserID = await User.findOne({ userID });
+  //   const existingUserEmail = await User.findOne({ email });
+  //   if (existingUserID || existingUserEmail) {
+  //     return { error: "User already exists" };
+  //   }
 
-    // Hash the password
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+  //   // Hash the password
+  //   const salt = bcrypt.genSaltSync(10);
+  //   const hashedPassword = bcrypt.hashSync(password, salt);
 
-    // Create a new user
-    const newUser = new User({
-      userID,
-      email,
-      password: hashedPassword,
-      isAdmin: false, // Ensure isAdmin is defined if it's necessary
-    });
+  //   // Create a new user
+  //   const newUser = new User({
+  //     userID,
+  //     email,
+  //     password: hashedPassword,
+  //     isAdmin: false, // Ensure isAdmin is defined if it's necessary
+  //   });
 
-    await newUser.save();
-    return { message: "User registered successfully" };
-  } catch (error) {
-    return { error: error.message };
-  }
+  //   await newUser.save();
+  //   return { message: "User registered successfully" };
+  // } catch (error) {
+  //   return { error: error.message };
+  // }
 };
 
 export const signInCredentials = async ({ userID, password }) => {
