085e87dac8e64b3f58020cba95c6429272c56d4c
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-19 16:30:02 +0530

    fixed and test

--- middleware.js
+++ middleware.js
@@ -2,4 +2,7 @@ export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  unstable_allowDynamic: [
+    "**/node_modules/@react-email*/**/*.mjs*",
+  ],
 };
