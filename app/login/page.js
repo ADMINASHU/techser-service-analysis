6e90d320773dab9b6bfa0706fe57c1d646d0f9a5
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-19 12:25:44 +0530

    login fixed

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,14 @@
 import LoginForm from "@/components/LoginForm";
 import React from "react";
-
-
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
-
+  const session = await auth();
+  const isAuthenticated = !!session?.user;
+  if (isAuthenticated) {
+    redirect("/");
+  }
   return <LoginForm />;
 };
 
