a5c1a5718f2adcec0ed53c4b6e9e9989d5b5fe60
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-19 13:59:26 +0530

    all fixed

--- app/api/control/route.js
+++ app/api/control/route.js
@@ -1,5 +1,5 @@
 import connectToServiceEaseDB from "../../../lib/serviceDB";
-import Point from '../../models/Point';
+import Point from '../../../lib/models/Point';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
