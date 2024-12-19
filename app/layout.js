b9e6aea12fd6a96e418714d9ab5c35201176f125
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-19 15:47:51 +0530

    seesion fixed

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default async function RootLayout({ children }) {
   const loggedUser = session?.user;
 
   return (
-    <DataProvider isAuthenticated={isAuthenticated} session={session}>
+    <DataProvider isAuthenticated={isAuthenticated} session={session} loggedUser={loggedUser}>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
           <Navbar isAuthenticated={isAuthenticated} session={session} loggedUser={loggedUser} />
