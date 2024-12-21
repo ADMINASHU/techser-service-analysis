f731ec30b2bc68fd6f54e827079252aaf4fce03f
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-21 13:39:25 +0530

    final auth completed

--- app/register/page.js
+++ app/register/page.js
@@ -1,8 +1,12 @@
-
 import RegisterForm from "@/components/RegisterForm";
-
-
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 const RegisterPage = async () => {
+  const session = await auth();
+  const isAuthenticated = !!session?.user;
+  if (isAuthenticated) {
+    redirect("/");
+  }
 
   return <RegisterForm />;
 };
