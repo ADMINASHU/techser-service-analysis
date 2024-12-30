9bf91de7c3e12aba9babebd0bd8cb8ab198b1680
Ashutosh Kumar <kashutosh02@gmail.com>
2024-12-30 12:38:27 +0530

    feat: update styling across components for improved layout and responsiveness

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,22 +1,15 @@
-
 import React from "react";
 import { auth } from "@/auth";
 import Profile from "@/components/Profile";
-
-
+import styles from "../../components/Profile.module.css";
 
 const ProfilePage = async () => {
   const session = await auth();
   const LoggedUserID = session?.user.userID;
 
-
   return (
-    <div 
-    style={{height:"80vh"}}
-    >
-      <h1>Profile</h1>
-
-      <Profile LoggedUserID= {LoggedUserID}/>  
+    <div className={styles.profileContainer}>
+      <Profile LoggedUserID={LoggedUserID} />
     </div>
   );
 };
